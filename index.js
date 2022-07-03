0778b22e61bd235e8a71c5e854d1abbad52337ae
CaraErskine <caraerskine@gmail.com>
2022-07-03 13:01:42 -0400

    commit tree 191ae8f1e9e272bb8edb820e99be874496dd959f

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+// the function below receives one argument, a string. 
+//returns true if a palindrome, false if not
+// 
+
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word.split("").reverse().join("")
+  === word) {
+    return true
+  } else {
+    return false
+  }
 }
+
+
 
 /* 
   Add your pseudocode here
-*/
+  // I need it to read the word forwards, like normal, and then backwards
+  //this will allow it to check if it is indeed a palindrome
+  //or use some kind of mirror function?
+
+  //with my code, I am saying that using the argument "word" with 
+  .split -will split the string into a list, so each word becomes it's own string.
+  .reverse -reverses an array in place, like
+in my pseudocode I was thinking "hey, can i mirror this?"
+  .join will join the elements in a string.
+
+*/ 
+
 
 /*
   Add written explanation of your solution here
